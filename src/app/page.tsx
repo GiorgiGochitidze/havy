cd2d27a0b6c86c743abebec96bd3e72098d9b5bc
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2026-03-13 12:42:10 +0400

    fixed gap between mini profile and main trackers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MiniProfile from "@/components/Profile/MiniProfile";
 
 export default function Home() {
   return (
-    <div className="flex gap-5">
+    <div className="flex">
       <main className="w-[calc(100%-400px)] h-auto gap-5 flex flex-col justify-start p-5">
         <Landing />
       </main>
